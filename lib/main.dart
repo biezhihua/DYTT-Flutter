9330a70365096ae7d74f5da666a0f592df9e9293
biezhihua <biezhihua@gmail.com>
2018-10-02 17:22:01 +0800

    Home Body

--- lib/main.dart
+++ lib/main.dart
@@ -26,38 +26,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(Strings.appName)),
-      drawer: DrawerLayout(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+    return DefaultTabController(
+      length: 10,
+      child: Scaffold(
+        drawer: DrawerLayout(),
+        appBar: AppBar(
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: [
+              Tab(text: Strings.tab1),
+              Tab(text: Strings.tab2),
+              Tab(text: Strings.tab3),
+              Tab(text: Strings.tab4),
+              Tab(text: Strings.tab5),
+              Tab(text: Strings.tab6),
+              Tab(text: Strings.tab7),
+              Tab(text: Strings.tab8),
+              Tab(text: Strings.tab9),
+              Tab(text: Strings.tab10),
+            ],
+          ),
+          title: Text(Strings.appName),
+        ),
+        body: TabBarView(
+          children: [
+            Container(),
+            Container(),
+            Container(),
+            Container(),
+            Container(),
+            Container(),
+            Container(),
+            Container(),
+            Container(),
+            Container()
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/strings.dart
+++ lib/strings.dart
@@ -2,4 +2,15 @@ class Strings {
   static const String appName = "电影天堂";
   static const String drawerLayoutAd = "随时随地，想看就看";
   static const String drawerLayoutGithub = "https://github.com/biezhihua";
+
+  static const String tab1 = "最新电影";
+  static const String tab2 = "综合电影";
+  static const String tab3 = "华语电影";
+  static const String tab4 = "欧美电影";
+  static const String tab5 = "日韩电影";
+  static const String tab6 = "华语电视";
+  static const String tab7 = "欧美电视";
+  static const String tab8 = "日韩电视";
+  static const String tab9 = "综艺";
+  static const String tab10 = "动漫";
 }
