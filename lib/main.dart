fa25477e6f1c47fc2454c3f2fdafa26e3077c1ee
biezhihua <biezhihua@gmail.com>
2018-10-02 16:44:46 +0800

    Feature DrawerLayout Header

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,27 @@
 import 'package:DYTT_FLUTTER/material_theme.dart';
+import 'package:DYTT_FLUTTER/strings.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: '电影天堂',
-      theme: new ThemeData(primaryColor: DyttMaterialTheme.colorPrimary),
-      home: new MyHomePage(title: '电影天堂'),
+    return MaterialApp(
+      title: DyttStrings.appName,
+      theme: ThemeData(
+        primaryColor: DyttMaterialTheme.colorPrimary,
+        primaryColorDark: DyttMaterialTheme.colorPrimaryDark,
+        accentColor: DyttMaterialTheme.colorAccent,
+      ),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -34,57 +35,70 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(DyttStrings.appName),
       ),
-      drawer: new Drawer(
+      drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-            new DrawerHeader(
-                child: new Column(
+            DrawerHeader(
+                child: Column(
                     mainAxisAlignment: MainAxisAlignment.end,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      new Text(
-                        '随时随地，想看就看',
-                        style: new TextStyle(color: Colors.white),
-                      ),
-                      new Text(
-                        'https://github.com/biezhihua',
-                        softWrap: false,
+                      Container(
+                          margin: EdgeInsets.only(bottom: 10.0),
+                          child: Image.asset(
+                            "images/icon.png",
+                            width: 64.0,
+                            height: 64.0,
+                          )),
+                      Container(
+                          margin: EdgeInsets.only(bottom: 10.0),
+                          child: Text(
+                            DyttStrings.drawerLayoutAd,
+                            style: TextStyle(color: Colors.white),
+                          )),
+                      Container(
+                        margin: EdgeInsets.only(bottom: 10.0),
+                        child: Text(
+                          DyttStrings.drawerLayoutGithub,
+                          softWrap: false,
+                          style: TextStyle(color: Colors.white),
+                        ),
                       )
                     ]),
                 decoration:
                     BoxDecoration(color: DyttMaterialTheme.colorPrimary)),
             ListTile(
-                leading: new Image.asset('images/film.png'),
-                title: new Text('电影天堂'),
+                leading: Image.asset('images/film.png'),
+                title: Text(DyttStrings.appName),
                 onTap: () {
                   Navigator.pop(context);
                 }),
           ],
         ),
       ),
-      body: new Center(
-        child: new Column(
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new Text(
+            Text(
               'You have pushed the button this many times:',
             ),
-            new Text(
+            Text(
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
           ],
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
+      floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: new Icon(Icons.add),
+        child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
---
+++ lib/strings.dart
@@ -0,0 +1,5 @@
+class DyttStrings {
+  static const String appName = "电影天堂";
+  static const String drawerLayoutAd = "随时随地，想看就看";
+  static const String drawerLayoutGithub = "https://github.com/biezhihua";
+}
